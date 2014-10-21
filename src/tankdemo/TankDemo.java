2cc6d9b6353a91f49c20d3e63321a8df424613c7
krieck <krieck@MN-KRIECK-L.mpls.digitalriver.com>
2014-10-21 08:07:01 -0500

    Add a little state machine logic.

--- src/tankdemo/TankDemo.java
+++ src/tankdemo/TankDemo.java
@@ -8,20 +8,23 @@ import processing.core.PApplet;
 public class TankDemo extends PApplet {
 
 	Tank tank;
-	float time;
+	float timePrevious;
+	int state = 0;
 	
 	public void setup() {
 		size(640,480);
 		tank = new Tank(this);
-		time = millis();
+		timePrevious = millis();
 	}
 
 	public void draw() {
 		background(128, 128, 128);
-		tank.run((millis() - time) / 1000.0f);
-		time = millis();
+		long timeNow = millis();
+		tank.run((timeNow - timePrevious) / 1000.0f);
+		timePrevious = timeNow;
 
 		if (keyPressed) {
+			state = 0;
 			if (key=='w') {
 				tank.setSpeed(40);
 			} else if (key == 's') {
@@ -30,10 +33,15 @@ public class TankDemo extends PApplet {
 				tank.turn(-30);
 			} else if (key == 'd') {
 				tank.turn(30);
+			} else if (key == '1') {
+				state = 1;
 			}
+		} else if (state == 1) {
+			// Do something different in state 1
 		} else {
 			tank.setSpeed(0);
 			tank.turn(0);
+			state = 0;
 		}
 	}
 }
